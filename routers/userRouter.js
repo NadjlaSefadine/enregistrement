c8cecc01fed0dd590508bceec87f4d691c6e76da
SalimaNousradine1 <salimanousradinehamad1@gmail.com>
2025-08-01 16:02:45 +0200

    ajout: ajout de route pour la connexion de l'utilisateur

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {register, verify} = require("../controllers/userController");
+const {register, login, verify} = require("../controllers/userController");
 const router = express.Router();
 
 // create un router aui permet d'ajouter un user
 router.post("/", register);
 router.post("/verify-email", verify);
+router.post("/login", login);
+
 
 module.exports = router;
